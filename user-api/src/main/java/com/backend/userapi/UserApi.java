dd06ebc16ef95c9e9a807f20408bd217ac8b25dc
Hegedüs Péter <peter.hegedus@kh.hu>
2025-05-19 22:43:07 +0200

    Refactor user API generation

--- user-api/src/main/java/com/backend/userapi/UserApi.java
+++
@@ -1,8 +0,0 @@
-package com.backend.userapi;
-
-import com.backend.userapi.api.DefaultApi;
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "user")
-public interface UserApi extends DefaultApi {
-}
