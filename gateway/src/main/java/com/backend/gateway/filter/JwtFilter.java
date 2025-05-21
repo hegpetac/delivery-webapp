3e8e03411b2626e8cbd8d334d9bf1567886a94ea
hegpetac <hegeduspeti2000@gmail.com>
2025-05-22 01:17:37 +0200

    Add filter and security config to gateway

---
+++ gateway/src/main/java/com/backend/gateway/filter/JwtFilter.java
@@ -0,0 +1,58 @@
+package com.backend.gateway.filter;
+
+import com.backend.jwtutil.JwtUtil;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.apache.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+
+@Component
+@Import(JwtUtil.class)
+@RequiredArgsConstructor
+public class JwtFilter implements WebFilter {
+    @Value("${deliveryWebapp.jwtSecret}")
+    private String jwtSecret;
+
+    private final JwtUtil jwtUtil;
+
+    private SecretKey getKey() {
+        return Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+    }
+
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+
+            try {
+                Jws<Claims> claims = jwtUtil.parseToken(token);
+                String username = claims.getPayload().getSubject();
+                String role = claims.getPayload().get("role", String.class);
+
+                exchange = exchange.mutate()
+                        .request(builder -> builder
+                                .header("X-User-Name", username)
+                                .header("X-User-Role", role))
+                        .build();
+            } catch (Exception e) {
+                return exchange.getResponse().setComplete();
+            }
+        }
+
+        return chain.filter(exchange);
+    }
+}
